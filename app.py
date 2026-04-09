07722a47218b385ecf8ce48473271ec0582eda31
Gouri-292 <gouriagrawal06@gmail.com>
2026-04-09 21:57:41 +0530

    logic updated

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/<name>/<int:birth_year>")
+def greet(name, birth_year):
+    current_year = datetime.now().year
+    age = current_year - birth_year
+    return f"<p>Hi {name}, you are {age} years old.</p>"
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-
-@app.route("/<name>")
-def greet(name):
-    return f"<p>Hi, {name}!</p>"
+# @app.route("/<name>")
+# def greet(name):
+#     return f"<p>Hi, {name}!</p>"
