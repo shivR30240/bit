7e42a1f0281ce465c2b827301b0c6b9b14a12653
shivr30 <shiv07raghu@gmail.com>
2026-04-09 15:46:47 +0530

    new edit by shiv with bith date

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +10,8 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/<name>")
-def greet(name):
-    return f"<p>Hello, {name}!</p>"
+@app.route("/<name>/<int:birth_year>")
+def greet(name, birth_year):
+    current_year = datetime.now().year
+    age = current_year - birth_year
+    return f"<p>Hi {name}, you are {age} years old.</p>"
